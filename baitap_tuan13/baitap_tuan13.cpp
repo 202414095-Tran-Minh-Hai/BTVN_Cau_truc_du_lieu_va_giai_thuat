df63d1aba0d6a83b7372fd5e6b847062a357c027
Tran Minh Hai <Hai.tm2414095@sis.hust.edu.vn>
2026-06-03 00:17:50 +0700

    them ham chen nam sinh

--- baitap_tuan13/baitap_tuan13.cpp
+++ baitap_tuan13/baitap_tuan13.cpp
@@ -15,6 +15,22 @@ struct Node {
     }
 };
 
+// Ham chen nam sinh vao cay
+Node* chenNamsinh(Node* nutHientai, int namsinh) {
+    if (nutHientai == NULL) {
+        return new Node(namsinh);
+    }
+    // Nho hon -> re trai
+    if (namsinh < nutHientai->data) {
+        nutHientai->left = chenNamsinh(nutHientai->left, namsinh);
+    }
+    // Lon hon hoac bang -> re phai (xu ly trung lap nam sinh)
+    else {
+        nutHientai->right = chenNamsinh(nutHientai->right, namsinh);
+    }
+    return nutHientai;
+}
+
 int main() {
   
     return 0;
