0a8224207b0d17a92bdccc08ebe6d5a73341d357
Tran Minh Hai <Hai.tm2414095@sis.hust.edu.vn>
2026-06-03 00:00:34 +0700

    tao node cay np

---
+++ baitap_tuan13/baitap_tuan13.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+// Dinh nghia cau truc mot nut tren cay
+struct Node {
+    int data; 
+    Node* left;
+    Node* right;
+
+    // tao Node moi
+    Node(int namsinh) {
+        data = namsinh;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+int main() {
+  
+    return 0;
+}
