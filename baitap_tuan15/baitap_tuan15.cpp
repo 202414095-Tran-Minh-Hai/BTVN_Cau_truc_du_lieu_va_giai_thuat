6381b5ae4a4c7737cd12de30aff82d1acc21c898
Tran Minh Hai <Hai.tm2414095@sis.hust.edu.vn>
2026-06-12 23:57:01 +0700

    khai bao node, them ham them canh. ham main test

--- baitap_tuan15/baitap_tuan15.cpp
+++ baitap_tuan15/baitap_tuan15.cpp
@@ -31,18 +31,54 @@ struct Hangdoi {
     }
 };
 
+
+//DANH SACH LIEN KET TP
+
+struct Node {
+    int iddinh; 
+    Node* next;
+};
+
+// Mang chua 11 con tro, moi con tro quan ly danh sach cac diem kề cua 1 thanh pho
+Node* danhsachke[11] = {NULL};
+
+// Ham tao mat xich moi
+Node* taonode(int v) {
+    Node* nutmoi = new Node();
+    nutmoi->iddinh = v;
+    nutmoi->next = NULL;
+    return nutmoi;
+}
+
+// Ham them 1 tuyen duong 2 chieu vao do thi
+void themcanh(int u, int v) {
+    // Them v vao danh sach cua u (Them vao dau danh sach)
+    Node* nutV = taonode(v);
+    nutV->next = danhsachke[u];
+    danhsachke[u] = nutV;
+
+    // Them u vao danh sach cua v
+    Node* nutU = taonode(u);
+    nutU->next = danhsachke[v];
+    danhsachke[v] = nutU;
+}
+
 int main() {
-    cout << "-TEST HANG DOI:" << endl;
-    Hangdoi q;
-
-    // cho Ha Noi (0) va Hai Duong (1) vao hang
-    q.them(0);
-    q.them(1);
-
-    cout << "Tp dung dau hang la: " << tenThanhPho[q.xemdau()] << endl;
-
-    q.xoa(); //kick Ha Noi
-    cout << "Sau khi xoa, tp dung dau la: " << tenThanhPho[q.xemdau()] << endl;
-
+    cout << "TEST DANH SACH LIEN KET- " << endl;
+    
+    // Thu them tuyen duong: Ha Noi (0) - Hai Duong (1) va Ha Noi (0) - Phu Ly (3)
+    themcanh(0, 1);
+    themcanh(0, 3);
+    
+    cout << "Cac thanh pho ke voi " << tenThanhPho[0] << " la: ";
+    
+    // Duyet danh sach lien ket cua Ha Noi de in ra
+    Node* tam = danhSachKe[0];
+    while (tam != NULL) {
+        cout << tenThanhPho[tam->iddinh] << " - ";
+        tam = tam->next;
+    }
+    cout << "\n=> ds oke" << endl;
+    
     return 0;
 }
